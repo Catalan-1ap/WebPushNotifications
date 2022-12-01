531829b944421a248eaa0a8870d26007bf9238b5
Yura Slobodenyuk <56219753+Catalan-1ap@users.noreply.github.com>
2022-12-01 11:56:31 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,22 +5,22 @@ import { UserProvider } from "../contexts/UserContext.jsx";
 import App from "./App.jsx";
 
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <UserProvider>
+            <NotificationsProvider>
+                <App/>
+            </NotificationsProvider>
+        </UserProvider>
+    </React.StrictMode>
+);
+
 try {
     navigator.serviceWorker
-    .register("./notificationsServiceWorker.js", {
-        scope: "/"
-    })
-    .then(() => navigator.serviceWorker.ready)
-
-    ReactDOM.createRoot(document.getElementById("root")).render(
-        <React.StrictMode>
-            <UserProvider>
-                <NotificationsProvider>
-                    <App/>
-                </NotificationsProvider>
-            </UserProvider>
-        </React.StrictMode>
-    );
+        .register("./notificationsServiceWorker.js", {
+            scope: "/"
+        })
+        .then(() => navigator.serviceWorker.ready)
 } catch (e) {
     console.error(e);
     alert(e);
