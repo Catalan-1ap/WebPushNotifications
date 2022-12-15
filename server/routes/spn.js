37858394766fa63631bcfbad2a552694b691843f
Yura Slobodenyuk <catalan.1ap.main@gmail.com>
2022-12-16 02:15:40 +0300

    server: spn version fix

--- server/routes/spn.js
+++ server/routes/spn.js
@@ -4,21 +4,21 @@ import path from "path";
 
 const router = express.Router();
 
-router.post("/v2/pushPackages/:website", async (req, res) => {
+router.post("/:version/pushPackages/:website", async (req, res) => {
 	const packagePath = path.resolve("./public/pushPackage.zip");
 
 	res.status(200).sendFile(packagePath);
 });
 
-router.post("/v2/devices/:device/registrations/:website", async (req, res) => {
+router.post("/:version/devices/:device/registrations/:website", async (req, res) => {
 	res.status(200).send();
 });
 
-router.delete("/v2/devices/:device/registrations/:website", async (req, res) => {
+router.delete("/:version/devices/:device/registrations/:website", async (req, res) => {
 	res.status(200).send();
 });
 
-router.delete("/v2/log", async (req, res) => {
+router.delete("/:version/log", async (req, res) => {
 	console.log(req.body);
 	res.status(200).send();
 });
