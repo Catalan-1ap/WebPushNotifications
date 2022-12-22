9e12ab6fdd1d1d4a2cffe61beca84c8cdd75b1b6
Yura Slobodenyuk <catalan.1ap.main@gmail.com>
2022-12-22 10:03:42 +0300

    server: apn send notification

--- client/contexts/NotificationsContext.jsx
+++ client/contexts/NotificationsContext.jsx
@@ -1,6 +1,5 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import useDeviceIdentifier from "../hooks/useDeviceIdentifier.js";
-import { checkSubscription } from "../services/api.js";
 import {
 	isNotificationsAvailable,
 	isSpnAvailable,
@@ -24,21 +23,6 @@ export function NotificationsProvider({ children }) {
 	const { user } = useUser();
 	const [subscribed, setSubscribed] = useState(false);
 	const deviceIdentifier = useDeviceIdentifier();
-
-	useEffect(() => {
-		async function impl() {
-			const isSubscribed = await checkSubscription(user.id);
-
-			setSubscribed(isSubscribed);
-			if (isSubscribed)
-				await subscribe();
-		}
-
-		if (!user)
-			return;
-
-		impl();
-	}, [user]);
 
 	async function subscribe() {
 		if (isSpnAvailable())
